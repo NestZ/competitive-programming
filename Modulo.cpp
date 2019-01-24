fee79ffc0b5e0afdd403e690c9ee924184c3b53b
NestZ <bobonono2009@windowslive.com>
2019-01-24 19:47:21 +0700

    011 Completed

---
+++ Modulo.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  int i,j,n[10],ans[10],answer = 0,num;
+
+  for(i=0;i<10;i++){
+    cin >> n[i];
+  }
+
+  for(i=0;i<10;i++){
+    ans[i] = n[i] % 42;
+    if(i==0)answer++;
+    else{
+      for(j=1;j<=i;j++){
+        if(ans[i] == ans[i-j])break;
+        else if(j == i)answer++;
+      }
+    }
+  }
+
+  cout << answer;
+
+  return 0;
+}
