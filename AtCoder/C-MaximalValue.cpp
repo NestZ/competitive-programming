c4f6b16032fee4a358de14635e35dcc9d0118487
NestZ <NestZ671@gmail.com>
2020-01-31 12:41:41 +0700

    :)

---
+++ AtCoder/C-MaximalValue.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+// make pair, tuple
+// __gcd(int, int)
+// bool isPrime(int n){
+//       if(n <= 1)return false;
+//       if(n <= 3)return true;
+//       if(n % 2 == 0 || n % 3 == 0)return false;
+//       for(int i = 5;i * i <= n;i += 6){
+//             if(n % i == 0 || n % (i + 2) == 0)return false;
+//       }
+//       return true;
+// }
+// int countSubString(string s, string temp){
+//       int ans = 0;
+//       size_t f = s.find(temp);
+//       while(f != string::npos){
+//             ans++;
+//             f = s.find(temp, f + 1);
+//       }
+//       return ans;
+// }
+
+int main(){
+    int n;cin >> n;
+    int sum = 0;
+    int arr[n + 5];
+    for(int i = 0;i < n - 1;i++)cin >> arr[i];
+    for(int i = 0;i < n;i++){
+        sum += min(arr[min(i, n - 2)], arr[max(0, i - 1)]);
+    }
+    cout << sum;
+}
