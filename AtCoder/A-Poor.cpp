3d8c46434e476acf643f25bb17a09043fbeae76d
NestZ <NestZ671@gmail.com>
2020-02-19 22:30:09 +0700

    :)

---
+++ AtCoder/A-Poor.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	set<int> s;
+	int a,b,c;cin >> a >> b >> c;
+	s.insert(a);
+	s.insert(b);
+	s.insert(c);
+	if(s.size() == 2)cout << "Yes";
+	else cout << "No";
+}
+// make pair, tuple
+// make pair, tuple
+// 
+// __gcd(int, int);
+// lcm = a * b / __gcd(a, b);
+// 
+// bool isPrime(int n){
+//       if(n <= 1)return false;
+//       if(n <= 3)return true;
+//       if(n % 2 == 0 || n % 3 == 0)return false;
+//       for(int i = 5;i * i <= n;i += 6){
+//             if(n % i == 0 || n % (i + 2) == 0)return false;
+//       }
+//       return true;
+// }
+// int countSubString(string s, string temp){
+//       int ans = 0;
+//       size_t f = s.find(temp);
+//       while(f != string::npos){
+//             ans++;
+//             f = s.find(temp, f + 1);
+//       }
+//       return ans;
+// }
+// 
+// #Count leading & trailing zero of an integer bits
+// __builtin_clz(n);
+// __builtin_clzll(n);
+//
+// #Count 1s bits of an integer
+// __builtin_popcount(n);
+// __builtin_popcountll(n);
+//
+// #Error checking
+// assert(n == 3);
+// 
+// #Error Printing
+// cerr << "Some thing";
+
+// #binary exponentiation
+// long long binpow(long long a, long long b) {
+//     long long res = 1;
+//     while (b > 0) {
+//         if (b & 1)
+//             res = res * a;
+//         a = a * a;
+//         b >>= 1;
+//     }
+//     return res;
+// }
