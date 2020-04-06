f0b03ac7715d4217f289804ceda630dfc667f8f5
NestZ <NestZ671@gmail.com>
2020-04-06 23:48:14 +0700

    :)

--- 379A.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-typedef long long int ll;
-
-int main(){
-	int a, b;cin >> a >> b;
-	cout << a + (a - 1) / (b - 1);
-}
--- 581A.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-typedef long long int ll;
-
-int main(){
-	int a, b;cin >> a >> b;
-	cout << min(a, b) << ' ' << (max(a, b) - min(a, b)) / 2;
-}
