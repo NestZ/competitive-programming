159f6390222917d5d7f76be8638b3107ee4d2b19
NestZ <NestZ671@gmail.com>
2021-06-20 21:19:27 +0700

    :)

---
+++ CodeForces/1537E1.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+typedef long long int ll;
+
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for(auto it = d.b;it != d.e;++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;cin >> n >> k;
+  string s;cin >> s;
+  vector<string> arr;
+  while(s.size()){
+    arr.push_back(s);
+    s.pop_back();
+  }
+  for(string &st : arr){
+    string ns = "";
+    for(int i = 0;i < k / (int)st.length();i++)
+      ns += st;
+    ns += st.substr(0, k % (int)st.length());
+    st = ns;
+  }
+  sort(arr.begin(), arr.end());
+  cout << arr[0] << endl;
+}
